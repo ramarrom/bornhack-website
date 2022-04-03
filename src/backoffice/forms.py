0d92990f3069487a54bf3717a9f5286a37152603
Víðir Valberg Guðmundsson <valberg@orn.li>
2022-04-03 13:16:46 +0200

    Choose which shop tickets to refund.

--- src/backoffice/forms.py
+++ src/backoffice/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.forms import modelformset_factory
 
 from program.models import Event, Speaker
-
-from shop.models import OrderProductRelation
+from tickets.models import ShopTicket
 
 
 class AddRecordingForm(forms.ModelForm):
@@ -158,21 +157,17 @@ class MobilePayCSVForm(forms.Form):
     )
 
 
-class OrderProductRelationRefundForm(forms.ModelForm):
+class ShopTicketRefundForm(forms.ModelForm):
     class Meta:
-        model = OrderProductRelation
-        fields = ["id", "refund_quantity"]
-        widgets = {
-            "id": forms.HiddenInput(),
-        }
+        fields = ["refund"]
 
-    refund_quantity = forms.IntegerField(initial=0, min_value=0)
+    refund = forms.BooleanField(required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.instance:
-            self.fields["refund_quantity"].max_value = self.instance.possible_refund
+        self.fields["refund"].label = self.instance.name or "Unnamed ticket"
 
 
-
-OrderProductRelationRefundFormSet = modelformset_factory(OrderProductRelation, form=OrderProductRelationRefundForm, extra=0)
+ShopTicketRefundFormSet = modelformset_factory(
+    ShopTicket, form=ShopTicketRefundForm, extra=0
+)
